ec19036a75a3f16e5771334f68c055adb902906a
Ishmeet <144150492+Jugglerish@users.noreply.github.com>
2023-10-06 02:27:46 +0530

    stack.py

--- stack.py
+++ stack.py
@@ -1,5 +1,3 @@
-stack = []
-
 class Stack:
     def __init__(self):
         self.stack = []
@@ -37,36 +35,28 @@ while True:
     choice = input("Please enter the choice\n1. Visit another link.\n2. Back.\n3. History\n4. Gets latest visited.\n5. Close the browser.\nEnter your choice here: ")
 
     print("-" * 60)
+    try:
+        choice = int(choice)
 
-    if choice.startswith("http://") or choice.startswith("https://"):
-
-        links_stacks.push(choice)
-    else:
-        try:
-            choice = int(choice)  
-
-            if choice == 1:
-                if not links_stacks.isEmpty():
-                    link = input("Enter the link:")
-                    links_stacks.push(link)
-                else:
-                    print("Please enter a link to initialize the browser history.")
-            elif choice == 2:
-                links_stacks.stack_pop()
-            elif choice == 3:
-                history = links_stacks.display()
-                if not history:
-                    print("History is empty.")
-                else:
-                    print("History:")
-                    for link in history:
-                        print(link)
-            elif choice == 4:
-                visited_latest = links_stacks.gettop()
-                print("Latest Visited link:", visited_latest)
-            elif choice == 5:
-                break
+        if choice == 1:
+            link = input("Enter the link:")
+            if link.startswith("http://") or link.startswith("https://"):
+                links_stacks.push(link)
             else:
-                print("Invalid choice")
-        except ValueError:
-            print("Invalid input. Please enter a valid choice (1-5) or a website URL.")
+                print("Please enter a valid link.")
+        elif choice == 2:
+            links_stacks.stack_pop()
+        elif choice == 3:
+            history = links_stacks.display()
+            print("History:")
+            for link in history:
+                print(link)
+        elif choice == 4:
+            visited_latest = links_stacks.gettop()
+            print("Latest Visited link:", visited_latest)
+        elif choice == 5:
+            break
+        else:
+            print("Invalid choice")
+    except ValueError:
+        print("Invalid input. Please enter a valid choice (1-5) or a website URL.")
