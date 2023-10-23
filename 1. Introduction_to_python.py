8c484aca5333bf2858c39bc750b1f4fe17b1d9dd
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-23 20:42:47 +0530

    Update 1. Introduction_to_python.py

--- 1. Introduction_to_python.py
+++ 1. Introduction_to_python.py
@@ -1,1 +1,19 @@
+# Write a Python program to get the values for a, b, c and evaluate the following expression delta = √b2-4ac.
 
+import math
+
+a = float(input("Enter the value of 'a': "))
+b = float(input("Enter the value of 'b': "))
+c = float(input("Enter the value of 'c': "))
+
+def calculate_the_delta(a, b, c):
+    delta = b**2 - 4 * a * c
+    return delta
+
+delta = calculate_the_delta(a, b, c)
+
+if delta >= 0:
+    square_root_of_delta = math.sqrt(delta)
+    print("Square Root of Delta (√Δ) =", square_root_of_delta)
+else:
+    print("The discriminant is negative, so there are no real solutions.")
