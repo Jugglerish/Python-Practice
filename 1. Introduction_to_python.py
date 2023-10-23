3dba1665f532b72965565c63d9e4378583e81ca6
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-23 20:46:26 +0530

    Update 1. Introduction_to_python.py

--- 1. Introduction_to_python.py
+++ 1. Introduction_to_python.py
@@ -17,3 +17,25 @@ if delta >= 0:
     print("Square Root of Delta (√Δ) =", square_root_of_delta)
 else:
     print("The discriminant is negative, so there are no real solutions.")
+
+# Find out the temperature for 12 months of the year and find out the year's average temperature
+
+temps = []
+
+for i in range(1, 13):
+    while True:
+        try:
+            t = float(input(f"Enter temperature for {i} month: "))
+            temps.append(t)
+            break
+        except ValueError:
+            print("Please enter a valid temperature (a number).")
+
+a = sum(temps) / len(temps)
+
+print("Monthly Temperatures:")
+for i, t in enumerate(temps, start=1):
+    print(f"Month {i}: {t}°C")
+
+print(f"Yearly Avg Temp: {a:.2f}°C")
+
