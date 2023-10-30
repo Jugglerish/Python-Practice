67d2815e714e5537b88485c99981a80efb116356
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-30 19:02:32 +0530

    Update 8. Strings.py

--- 8. Strings.py
+++ 8. Strings.py
@@ -1,1 +1,35 @@
+# 1. Create your multiline string and show how to print it.
+lyrics =  '''Cause we were just kids when we fell in love
+Not knowing what it was
+I will not give you up this time
+But darling, just kiss me slow
+Your heart is all I own
+And in your eyes, you're holding mine'''
+print(lyrics)
+lyrics1 = "Cause we were just kids when we fell in love \n Not knowing what it was \nI will not give you up this time \nBut darling, just kiss me slow \nYour heart is all I own \nAnd in your eyes, you're holding mine"
+print(lyrics1)
 
+# 2. Create a program that accepts a string and displays each word's length and the length of the string.
+stringinput = input("Enter your String here: ")
+striglen = len(stringinput)
+print("String character's length: ", striglen)
+stringwordlist = stringinput.split( ) 
+stringwordlist = list(stringwordlist)
+stringwordlen = len(stringwordlist )
+print("String word length: ", stringwordlen, "\n" )
+
+for i in stringwordlist:
+    print(f"{i} length: ", len(i))
+
+# 3. Write a program that will count the number of lowercase and uppercase character in a user- supplied string.
+stringinput = input("Enter your String here: ")
+counterlower = 0
+counterupper = 0
+for i in stringinput:
+    if i == i.lower():
+        counterlower += 1
+    elif i == i.upper():
+        counterupper += 1
+    else:
+        print("Invalid Input")
+print(f"Total lowercase character are: {counterlower}, Total uppercase characters are: {counterupper}")
