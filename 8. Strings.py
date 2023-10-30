d00ee4c9d1e2ba8510c84ba84abe040292e81bf7
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-30 19:10:11 +0530

    Update 8. Strings.py

--- 8. Strings.py
+++ 8. Strings.py
@@ -33,3 +33,19 @@ for i in stringinput:
     else:
         print("Invalid Input")
 print(f"Total lowercase character are: {counterlower}, Total uppercase characters are: {counterupper}")
+
+
+# 4. Create a program that will count the length of the given string without relying on an in-built function. Given string:
+given_string = "Jack and Jill went up the hill. To fetch a pail of water."
+counter = 0
+for i in given_string:
+    counter += 1
+print(f"Total characters length is: {given_string}")
+
+word_counter = 0
+wordsplit = given_string.split()
+wordlist = list(wordsplit)
+for j in wordlist:
+    word_counter += 1
+
+print(f"Character count: {counter} \nWord count: {word_counter}")
