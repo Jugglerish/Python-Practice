5b0029f198d55ea9a507f9e30f0f63dba06b7a21
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-07 16:55:25 +0530

    14. Advanced_class.py

--- 14. Advanced_class.py
+++ 14. Advanced_class.py
@@ -1,1 +1,16 @@
+class IncLst:
+    def __init__(self, d):
+        self.d = d
+
+    def __pos__(self):
+        l = [x + 2 for x in self.d]
+        return IncLst(l)
+
+    def __str__(self):
+        return str(self.d)
+
+ol = [1, 3, 5, 7]
+inc = +IncLst(ol)
+print("Original List:", ol)
+print("Incremented List:", inc)
 
