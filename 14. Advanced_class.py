c9611cdb9007e14ae53419bdf129e28f430c283b
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-07 17:00:20 +0530

    Update 14. Advanced_class.py

--- 14. Advanced_class.py
+++ 14. Advanced_class.py
@@ -15,3 +15,36 @@ inc = +IncLst(ol)
 print("Original List:", ol)
 print("Incremented List:", inc)
 
+# 2. Using the concept of operator overloading, implement the operator "*" to perform matrix multiplication for any 3X3 matrices in Python where matrices should be stored in list format.
+class M:
+    def __init__(s, e):
+        if len(e) != 3 or any(len(r) != 3 for r in e):
+            raise ValueError("Invalid matrix size")
+        s.e = e
+
+    def __str__(s):
+        return "\n".join([" ".join(map(str, r)) for r in s.e])
+
+    def __mul__(s, o):
+        if not isinstance(o, M):
+            raise ValueError("Invalid multiplication")
+
+        r = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    r[i][j] += s.e[i][k] * o.e[k][j]
+
+        return M(r)
+
+m1 = M([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m2 = M([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+
+r = m1 * m2
+print("Matrix 1:")
+print(m1)
+print("\nMatrix 2:")
+print(m2)
+print("\nMatrix 1 * Matrix 2:")
+print(r)
