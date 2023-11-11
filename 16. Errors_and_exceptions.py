158fed742c7bfb46e785343c4bd9774a6b446c32
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-11 16:48:53 +0530

    Update 16. Errors_and_exceptions.py

--- 16. Errors_and_exceptions.py
+++ 16. Errors_and_exceptions.py
@@ -1,1 +1,10 @@
+# 1. Write a program that asks for input from the user and incorporate value error exception handling into it.
+while 1:
+    try:
+        n = int(input("Enter a number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+print("You entered:", n)
 
