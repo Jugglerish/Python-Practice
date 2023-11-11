21dea377c9b3b2e6ffeb4dc83d493fbc2ebe3926
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-12 02:11:55 +0530

    Update 17. Debuging_and_Testing.py

--- 17. Debuging_and_Testing.py
+++ 17. Debuging_and_Testing.py
@@ -18,3 +18,22 @@ pdb.set_trace()
 pns = pn(n)
 print(f"pRiMe numbeRS up to {n}: {pns}")
 
+
+# 2. Write a function for finding a perfect number and use Pdb to debug it.
+import pdb
+
+def pfct_nmbr(num):
+    pdb.set_trace()
+    sum_divs = 0
+    for i in range(1, num):
+        if num % i == 0:
+            sum_divs += i
+    return sum_divs == num
+
+if __name__ == "__main__":
+    input_num = int(input("Enter a number: "))
+    result = pfct_nmbr(input_num)
+    if result:
+        print(f"The number {input_num} is a perfect number.")
+    else:
+        print(f"The number {input_num} is not a perfect number.")
