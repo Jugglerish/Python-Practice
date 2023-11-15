24ae673ae4d1d590ee14ddf53379708a78879ea5
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-15 19:29:05 +0530

    Update 18.  File_handling.py

--- 18.  File_handling.py
+++ 18.  File_handling.py
@@ -1,1 +1,13 @@
+# Use the following text files for the programming exercise.
 
+# specimenl.txt
+# Fintech, a combination of the terms "financial" and technology," refers to businesses that use technology to enhance or automate financial services and processes.
+
+# specimen2.txt
+# Python is one of the famous programming language.
+# Python is used in the Financial Analyses and other processing.
+
+# specimen3.txt
+# Python is useful in storing and retrieving data into a file including database.
+# The data analysis is made easier in Python which make add fuels to its popularity.
+# Python adopts to any upcoming trend in IT industry.
