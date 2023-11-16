a5ab9c797b4e2d66f558246623c80d568502eb0a
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-16 23:21:48 +0530

    Update 20. data_analytics.py

--- 20. data_analytics.py
+++ 20. data_analytics.py
@@ -5,3 +5,18 @@ oddele = xyz[xyz % 2!= 0]
 eveele = xyz[xyz %  2 == 0]
 print("Odd elements:", oddele)
 print("Even elements:", eveele)
+
+
+
+# 2. Find the sum of an array.
+xyz = [1, 2, 3, 4, 5]
+array_sum = sum(xyz)
+print(array_sum)
+
+#using np
+import numpy as np
+xyz =  np.array([1,2,3,4,5,6,7,8,9])
+oddele = xyz[xyz % 2!= 0]
+eveele = xyz[xyz %  2 == 0]
+print("Odd elements:", oddele)
+print("Even elements:", eveele)
