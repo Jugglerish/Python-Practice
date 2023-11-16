914f69364c6cc9236b7c47540246135399dc8fb5
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-16 23:19:39 +0530

    Update 20. data_analytics.py

--- 20. data_analytics.py
+++ 20. data_analytics.py
@@ -1,1 +1,7 @@
-
+# 1. Find the odd and even elements in an array.
+import numpy as np
+xyz =  np.array([1,2,3,4,5,6,7,8,9])
+oddele = xyz[xyz % 2!= 0]
+eveele = xyz[xyz %  2 == 0]
+print("Odd elements:", oddele)
+print("Even elements:", eveele)
